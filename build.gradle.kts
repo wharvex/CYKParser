ce1154d90b960b39e49a1fc3548ae991cd464508
Tim <81175852+wharvex@users.noreply.github.com>
2024-12-15 20:03:23 -0500

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("org.example.Main")
+}
